93ee227ff4e1be20e88618a1ed0c72517f1c9133
Igor Souza Martins <igor.souza@devninjas.com.br>
2019-10-31 13:41:46 -0300

    novo teste

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -12,3 +12,12 @@ it('sum three numbers', () => {
 
   expect(a + b + c).toBe(30)
 })
+
+it('sum four numbers', () => {
+  const a = 5
+  const b = 10
+  const c = 15
+  const d = 20
+
+  expect(a + b + c + d).toBe(50)
+})
